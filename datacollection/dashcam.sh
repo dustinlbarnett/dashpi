e517f2c47986352f41f07a2e7cc2a61ea8f9678a
dustinlbarnett <dustinlbarnett@gmail.com>
2015-04-25 17:49:03 -0700

    Changes

--- datacollection/dashcam.sh
+++ datacollection/dashcam.sh
@@ -11,8 +11,43 @@ if ! lsusb | grep -wq "PL2303"; then
     echo "$DATETIME GPS detected"
 fi
 
-# Get GPS Time using python script. 
-sudo ./gpstime.sh
+# Set clock to time from GPS. 
+ATTEMPT=0
+GPSFIX=0
+TIMESET=0
+MODE=0
+
+while [ $TIMESET -lt 1 ] 
+        do
+        while [ $ATTEMPT -lt 12 ]
+                do
+                if [ $GPSFIX -lt 1 ]
+                then
+                        while [ $MODE -lt 2 ]
+                        do
+                                MODE=`gpspipe -w | head -10 | grep TPV | sed -r 's/.*"mode":([0-9]).*/\1/' | head -1`
+                                GPSFIX=$MODE
+                                let "ATTEMPT=ATTEMPT+1"
+                                TIMESET=10
+                                sleep 5
+                        done
+                else
+                        echo "GPS fixed, getting time..."       
+                        GPSDATE=`gpspipe -w | head -10 | grep TPV | sed -r 's/.*"time":"([^"]*)".*/\1/' | head -1`
+                        echo "date -s $GPSDATE"
+                        sudo date -s $GPSDATE
+                        TIMESET=1
+                        ATTEMPT=100
+                fi
+        done
+done
+
+if [ $ATTEMPT = 100 ]
+then
+        echo "Success"
+else
+        echo "Unable to get time from GPS device."
+fi
 
 #set date/time variables
 TIMESTAMP=$(date +"%H-%M")
@@ -24,7 +59,7 @@ mkdir -p "$DESTINATION"/dashcam
 mkdir -p "$DESTINATION"/dashcam/images/"$DATETIME"
 mkdir -p "$DESTINATION"/dashcam/gps_tracks
 
-# collect gps stream. kill gpspip if running
+# collect gps stream. kill gpspipe if running
 killall gpspipe
 gpspipe -r -o "$DESTINATION"/dashcam/gps_tracks/"$DATETIME".nmea &
 
